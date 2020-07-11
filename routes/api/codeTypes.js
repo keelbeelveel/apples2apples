d164dafb81161bac7badf7cee89ee8023a391147
keelbeelveel <k.belveal17@gmail.com>
2020-07-11 17:21:57 -0400

    new req handling logic -- (with corrected express syntax)

--- routes/api/codeTypes.js
+++ routes/api/codeTypes.js
@@ -1,17 +1,18 @@
-// Script modified: Sat July 11, 2020 @ 04:37:16 EDT
+// Script modified: Sat July 11, 2020 @ 05:14:55 EDT
 const express = require('express');
 const router = express.Router();
 
-router.post('/api/codeTypes', (req, res) => {
+router.post('/', (req, res) => {
     var status = 200;
-    resBody = `Your POST resquest was recieved: ${JSON.stringify(req)}`;
-    res.status(status).send(resBody);
+    resBody = "Your POST resquest was recieved: ";
+    res.status(status).send(resBody).json(req.body);
 });
 
-router.get('/api/codeTypes', (req, res) => {
+router.get('/', (req, res) => {
+    console.log("API/Code Get request");
     var status = 200;
-    resBody = `Your POST resquest was recieved: ${JSON.stringify(req)}`;
-    res.status(status).send(resBody);
+    resBody = "Your POST resquest was recieved: ";
+    res.status(status).send(resBody).json(req.body);
 });
 
 module.exports = router;
