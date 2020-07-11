dcdc1a97bcb4cd0988957e4189c55a1da3def034
keelbeelveel <k.belveal17@gmail.com>
2020-07-11 17:16:08 -0400

    fixed db_connection

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// Script modified: Sat July 11, 2020 @ 04:38:29 EDT
+// Script modified: Sat July 11, 2020 @ 05:15:48 EDT
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
 
 //IMPORT ROUTES
-codeTypeRoute = require('./routes/api/codeTypes');
-app.use('/api/codeTypes', codeTypeRoute);
-
-//Conntext to DB
+codeTypeRoute = require('./routes/api/codeTypes');             
+app.use('/api/codeTypes', codeTypeRoute);                      
+                                                               
+//Connects to DB                                               
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
     console.log('connected.')
-);
+);                                                             
 
 app.listen(3000);
