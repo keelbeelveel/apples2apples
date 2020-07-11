8ae2050edf638f81fb29403522bc6b908472605f
keelbeelveel <k.belveal17@gmail.com>
2020-07-11 17:39:49 -0400

    express is making me dishappy

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-// Script modified: Sat July 11, 2020 @ 05:15:48 EDT
+// Script modified: Sat July 11, 2020 @ 05:25:05 EDT
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
 
 //IMPORT ROUTES
-codeTypeRoute = require('./routes/api/codeTypes');             
-app.use('/api/codeTypes', codeTypeRoute);                      
-                                                               
-//Connects to DB                                               
+codeTypeRoute = require('./routes/api/codeTypes');
+app.use('/api/codeTypes', codeTypeRoute);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+//Connects to DB
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
     console.log('connected.')
-);                                                             
+);
 
 app.listen(3000);
--- routes/api/codeTypes.js
+++ routes/api/codeTypes.js
@@ -1,18 +1,29 @@
-// Script modified: Sat July 11, 2020 @ 05:14:55 EDT
+// Script modified: Sat July 11, 2020 @ 05:37:22 EDT
 const express = require('express');
+const app = express();
 const router = express.Router();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 router.post('/', (req, res) => {
+    console.log(req);
     var status = 200;
-    resBody = "Your POST resquest was recieved: ";
-    res.status(status).send(resBody).json(req.body);
+    reqIp = req.ip;
+    resBody = `Your GET resquest was recieved: ${reqIp}`;
+    res.status(status).send(resBody);
+    console.log(req.body);
 });
 
 router.get('/', (req, res) => {
+    console.log(req);
     console.log("API/Code Get request");
     var status = 200;
-    resBody = "Your POST resquest was recieved: ";
-    res.status(status).send(resBody).json(req.body);
+    reqIp = req.ip;
+    resBody = `Your GET resquest was recieved: ${reqIp}`;
+    res.status(status).send(resBody);
+    console.log("BODY:");
+    console.log(req.body);
 });
 
 module.exports = router;
